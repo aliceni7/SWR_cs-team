b1fd4cc349f32443327d8ad37587443f178e6bec
rleongPd5 <rleong00@stuy.edu>
2020-07-11 00:12:31 -0400

    added styling for the grid

--- maze.js
+++ maze.js
@@ -46,26 +46,38 @@ function moveDown(){
     avatar.style.top=parseInt(avatar.style.top)+10 +'px';
 }
 
-function makeBlankMaze(rows = 8, cols = 8){
+function makeBlankBacktrackMaze(rows = 8, cols = 8){
     var table = document.createElement("table");
     var tbody = document.createElement("tbody");
     var tablerow;
     var tabledata;
-    for (var r = 0; r <= rows; r++){
+    for (var r = 0; r < rows; r++){
         tablerow = document.createElement("tr");
         for (var c = 0; c < cols; c++){
           tabledata = document.createElement("td");
+          tabledata.id = "cell_r" + r + "_c" + c;
+          if (r === 0){
+            tabledata.classList.add("topborder");
+          }
+          if (c === 0){
+            tabledata.classList.add("leftborder");
+          }
+          if (r === rows - 1){
+            tabledata.classList.add("botborder");
+          }
+          if (c === cols - 1){
+            tabledata.classList.add("rightborder");
+          }
           tablerow.appendChild(tabledata);
         }
-        console.log(tablerow);
         tbody.appendChild(tablerow);
     }
     table.appendChild(tbody);
     document.getElementById("maze_container").appendChild(table);
 }
-makeBlankMaze();
+makeBlankBacktrackMaze();
 
 function mazeGenRecursiveDivision(rows = 8, cols = 8){
-
+  document.getElementBy
 }
 window.onload=init;
