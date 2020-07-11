d19ba63caa9c22ecddc17097db92ac4233653f2a
rleongPd5 <rleong00@stuy.edu>
2020-07-11 01:34:00 -0400

    Added getCoords, removed border for start and end cell and finished step 1 of pseudocode

--- maze.js
+++ maze.js
@@ -46,6 +46,7 @@ function moveDown(){
     avatar.style.top=parseInt(avatar.style.top)+10 +'px';
 }
 
+// Creates an empty grid with start and end cells in HTML file and returns the maze
 function makeBlankBacktrackMaze(rows = 8, cols = 8){
     var table = document.createElement("table");
     var tbody = document.createElement("tbody");
@@ -74,49 +75,90 @@ function makeBlankBacktrackMaze(rows = 8, cols = 8){
     }
     table.appendChild(tbody);
     document.getElementById("maze_container").appendChild(table);
+    return makeStartEndCells(rows, cols);
 }
-makeBlankBacktrackMaze();
 
-/* returns a random integer between 0 and the given maximum (noninclusive) parameter*/
+// returns a random integer between 0 and the given maximum (noninclusive) parameter
 function getRandomInt(max){
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function mazeGenBacktracking(rows = 8, cols = 8){
+// Determines a start and end cell and returns the maze
+function makeStartEndCells(rows = 8, cols = 8){
   var maze = document.getElementsByTagName("tbody")[0];
   var startCell, endCell;
-  console.log(maze);
+  // console.log(maze);
+  // determine start and end of maze
   var mazeOrient = getRandomInt(4);
   switch(mazeOrient){
     case 0:
       var leftBorder = maze.getElementsByClassName("leftborder");
       startCell = leftBorder[getRandomInt(rows)];
+      startCell.style.borderLeft = "0px";
       var rightBorder = maze.getElementsByClassName("rightborder");
       endCell = rightBorder[getRandomInt(rows)];
+      endCell.style.borderRight = "0px";
       break;
     case 1:
       var rightBorder = maze.getElementsByClassName("rightborder");
       startCell = rightBorder[getRandomInt(rows)];
+      startCell.style.borderRight = "0px";
       var leftBorder = maze.getElementsByClassName("leftborder");
       endCell = leftBorder[getRandomInt(rows)];
+      endCell.style.borderLeft = "0px";
       break;
     case 2:
       var topBorder = maze.getElementsByClassName("topborder");
       startCell = topBorder[getRandomInt(cols)];
+      startCell.style.borderTop = "0px";
       var botBorder = maze.getElementsByClassName("botborder");
       endCell = botBorder[getRandomInt(cols)];
+      endCell.style.borderBottom = "0px";
       break;
     default:
       var botBorder = maze.getElementsByClassName("botborder");
       startCell = botBorder[getRandomInt(cols)];
+      startCell.style.borderBottom = "0px";
       var topBorder = maze.getElementsByClassName("topborder");
       endCell = topBorder[getRandomInt(cols)];
+      endCell.style.borderTop = "0px";
       break;
   }
   startCell.classList.add("start");
   endCell.classList.add("end");
+  return maze;
 }
-function backtrackHelper(){
+// Given a cell returns an object with the given cell's row and col number
+function getCoords(cell){
+  var id = cell.id;
+  var row = id.slice(cell.id.indexOf("_") + 2, cell.id.lastIndexOf("_"));
+  // console.log(row);
+  var col = id.slice(cell.id.lastIndexOf("_") + 2, id.length);
+  // console.log(col);
+  return {
+    "row": row,
+    "col": col
+  }
+}
+/* Pseudocode
+    Choose the initial cell, mark it as visited and push it to the stack
+      While the stack is not empty
+        Pop a cell from the stack and make it a current cell
+        If the current cell has any neighbours which have not been visited
+          Push the current cell to the stack
+          Choose one of the unvisited neighbours
+          Remove the wall between the current cell and the chosen cell
+          Mark the chosen cell as visited and push it to the stack
+*/
+function mazeGenBacktracking(rows = 8, cols = 8){
+  var maze = makeBlankBacktrackMaze(rows, cols);
+  var start = document.getElementsByClassName("start")[0];
+  console.log(getCoords(start));
+  var stack = [];
+  stack.push(start);
+  console.log(stack);
+}
+function backtrackHelper(cell){
 
 }
 mazeGenBacktracking();
