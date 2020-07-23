badd8241a7b515a76042b3e163dfb75a18e390e7
rleongPd5 <rleong00@stuy.edu>
2020-07-23 02:41:27 -0400

    Attempted drag and drop

---
+++ jigsaw.js
@@ -0,0 +1,35 @@
+/* https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
+drag and drop events
+  drag, dragstart, dragenter, dragexit, dragleave, dragover
+drag and drop interface
+  dataTransfer
+*/
+
+var dragItem = document.getElementById("dragElement");
+var dropLoc = document.getElementById("jigsaw_container");
+
+// this event will be fired when the user starts dragging
+dragItem.ondragstart = function(event){
+  event.dataTransfer.setData('key', event.target.id);
+  console.log("Dragging");
+}
+
+// this event will be fired when an element selection is being dragged over to a valid drop location
+dropLoc.ondragover = function(event){
+  event.preventDefault();
+  console.log("Drag over");
+}
+
+// this event will be fired when you drop the dragged item on a valid drop location
+dropLoc.ondrop = function(event){
+  var dropItem = event.dataTransfer.getData('key');
+  event.preventDefault();
+  console.log(event);
+  console.log("Dropped");
+  console.log(dropItem);
+  var myElement = document.getElementById(dropItem);
+  console.log(myElement);
+  var myNewElement = document.createElement('img');
+  myNewElement.src = myElement.src;
+  dropLoc.appendChild(myNewElement);
+}
