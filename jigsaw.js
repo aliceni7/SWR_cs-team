77f2084474ffe47402c8920aef8662a629dfdc80
rleongPd5 <rleong00@stuy.edu>
2020-08-06 02:18:02 -0400

    Got modal to work, but can't get images to drag and be displayed properly when canvas height and width is set to 100%

--- jigsaw.js
+++ jigsaw.js
@@ -1,28 +1,171 @@
-var dragItem = document.getElementById('dragElement');
-var dropLoc = document.getElementById('jigsaw_container');
+// Get modal element
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.getElementsByClassName('closeBtn');
 
-// this event will be fired when the user starts dragging
-dragItem.ondragstart = function (event) {
-  event.dataTransfer.setData('key', event.target.id);
-  console.log('Dragging');
-};
+modalBtn.addEventListener('click', openModal);
 
-// this event will be fired when an element selection is being dragged over to a valid drop location
-dropLoc.ondragover = function (event) {
-  event.preventDefault();
-  console.log('Drag over');
-};
+closeBtn[0].addEventListener('click', closeModal);
 
-// this event will be fired when you drop the dragged item on a valid drop location
-dropLoc.ondrop = function (event) {
-  var dropItem = event.dataTransfer.getData('key');
-  event.preventDefault();
-  console.log(event);
-  console.log('Dropped');
-  console.log(dropItem);
-  var myElement = document.getElementById(dropItem);
-  console.log(myElement);
-  var myNewElement = document.createElement('img');
-  myNewElement.src = myElement.src;
-  dropLoc.appendChild(myNewElement);
+//function to open modalBtn
+function openModal(){
+  modal.style.display = 'block';
+}
+
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+function outsideClick(e){
+  if (e.target == modal){
+    modal.style.display = 'none';
+  }
+}
+
+// Dragging picture around canvas
+// canvas related vars
+var canvas=document.getElementById("puzzle_canvas");
+var ctx=canvas.getContext("2d");
+// canvas.width=window.innerWidth;
+// canvas.height=window.innerHeight;
+var cw=canvas.width;
+var ch=canvas.height;
+canvas.style.border='1px solid red';
+
+// used to calc canvas position relative to window
+function reOffset(){
+    var BB=canvas.getBoundingClientRect();
+    offsetX=BB.left;
+    offsetY=BB.top;
+}
+var offsetX,offsetY;
+reOffset();
+window.onscroll=function(e){ reOffset(); }
+window.onresize=function(e){ reOffset(); }
+canvas.onresize=function(e){ reOffset(); }
+canvas.onclick=function(e){ reOffset(); }
+
+// save relevant information about shapes drawn on the canvas
+var shapes=[];
+
+// drag related vars
+var isDragging=false;
+var startX,startY;
+
+// hold the index of the shape being dragged (if any)
+var selectedShapeIndex;
+
+// load the image
+var toucan=new Image();
+toucan.onload=function(){
+    // define one image and save it in the shapes[] array
+    shapes.push( {x:30, y:10, width:toucan.width, height:toucan.height, image:toucan} );
+    // draw the shapes on the canvas
+    drawAll();
+    // listen for mouse events
+    canvas.onmousedown=handleMouseDown;
+    canvas.onmousemove=handleMouseMove;
+    canvas.onmouseup=handleMouseUp;
+    canvas.onmouseout=handleMouseOut;
 };
+// put your image src here!
+toucan.src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/-_panoramio_-_Basa_Roland.jpg/240px--_panoramio_-_Basa_Roland.jpg';
+
+
+// given mouse X & Y (mx & my) and shape object
+// return true/false whether mouse is inside the shape
+function isMouseInShape(mx,my,shape){
+    // is this shape an image?
+    if(shape.image){
+        // this is a rectangle
+        var rLeft=shape.x;
+        var rRight=shape.x+shape.width;
+        var rTop=shape.y;
+        var rBott=shape.y+shape.height;
+        // math test to see if mouse is inside image
+        if( mx>rLeft && mx<rRight && my>rTop && my<rBott){
+            return(true);
+        }
+    }
+    // the mouse isn't in any of this shapes
+    return(false);
+}
+
+function handleMouseDown(e){
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+    // calculate the current mouse position
+    startX=parseInt(e.clientX-offsetX);
+    startY=parseInt(e.clientY-offsetY);
+    // test mouse position against all shapes
+    // post result if mouse is in a shape
+    for(var i=0;i<shapes.length;i++){
+        if(isMouseInShape(startX,startY,shapes[i])){
+            // the mouse is inside this shape
+            // select this shape
+            selectedShapeIndex=i;
+            // set the isDragging flag
+            isDragging=true;
+            // and return (==stop looking for
+            //     further shapes under the mouse)
+            return;
+        }
+    }
+}
+
+function handleMouseUp(e){
+    // return if we're not dragging
+    if(!isDragging){return;}
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+    // the drag is over -- clear the isDragging flag
+    isDragging=false;
+}
+
+function handleMouseOut(e){
+    // return if we're not dragging
+    if(!isDragging){return;}
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+    // the drag is over -- clear the isDragging flag
+    isDragging=false;
+}
+
+function handleMouseMove(e){
+    // return if we're not dragging
+    if(!isDragging){return;}
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+    // calculate the current mouse position
+    mouseX=parseInt(e.clientX-offsetX);
+    mouseY=parseInt(e.clientY-offsetY);
+    // how far has the mouse dragged from its previous mousemove position?
+    var dx=mouseX-startX;
+    var dy=mouseY-startY;
+    // move the selected shape by the drag distance
+    var selectedShape=shapes[selectedShapeIndex];
+    selectedShape.x+=dx;
+    selectedShape.y+=dy;
+    // clear the canvas and redraw all shapes
+    drawAll();
+    // update the starting drag position (== the current mouse position)
+    startX=mouseX;
+    startY=mouseY;
+}
+
+// clear the canvas and
+// redraw all shapes in their current positions
+function drawAll(){
+    ctx.clearRect(0,0,cw,ch);
+    for(var i=0;i<shapes.length;i++){
+        var shape=shapes[i];
+        if(shape.image){
+            // it's an image
+            ctx.drawImage(shape.image,shape.x,shape.y, shape.width, shape.height);
+        }
+    }
+}
